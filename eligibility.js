2ff4a53281549dededb5ec9a8fd37c6eecc84b58
mayor04 <ssshobanke@gmail.com>
2023-04-19 08:54:48 +0100

    DELETE FILE

--- eligibility.js
+++
@@ -1,8 +0,0 @@
-let age = prompt("What is your age", 18);
-let x = 18 - age
-
-if (age >= 18) {
-    alert("You're eligible to vote");
-} else {
-    alert("Sorry, you're not eligible to vote yet. Come back in " + x + " years time");
-}
